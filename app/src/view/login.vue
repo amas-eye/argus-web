e357101580e7fb0a226b91900b43151ff65a1f95
EaconTang <tyingk@163.com>
2018-03-29 13:45:18 +0800

    update default login page

--- app/src/view/login.vue
+++ app/src/view/login.vue
@@ -4,15 +4,13 @@
     <div class="form">
       <div class='headtab'>
         <div class="ch-title">
-          大数据智能监控平台
+          Amas
         </div>
         <!-- <div class="en-title">
-                                      WiFi Bid Data Platform
+                                      Amas
                                     </div> -->
       </div>
-      <!-- <div class="welcom">
-                                    欢迎使用WiFi大数据平台</br>请输入用户信息
-                                  </div> -->
+
       <div class="login-form">
         <div class="login-text">
           <i class="icon-account"></i>
@@ -27,7 +25,7 @@
                                   <input v-model="login_info.yzm" type='text' placeholder="请输入验证码" />
                                 </div>
                                 <img style="width:7rem; height:3rem;" @click="getcore()" :src="coresrc"></img>
-                                 <span v-on:click="$router.push('forget_password')">忘记密码?</span> 
+                                 <span v-on:click="$router.push('forget_password')">忘记密码?</span>
                               </div> -->
         <div class="login-buttonground">
           <!--mt-button class='login-button' v-on:click="login">登录</mt-button-->
@@ -35,7 +33,13 @@
         </div>
       </div>
     </div>
-    <div class="copyright">广州优亿信息科技有限公司</div>
+
+      <div class="copyright">
+        <a href="https://github.com/amas-eye/amas">
+        <svg class="octicon octicon-mark-github" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
+        </a>
+      </div>
+
   </div>
 </template>
 
@@ -67,10 +71,10 @@
           res => {
              console.info('登录',res)
              this.$router.push('/navBar/generalOverview')
-            
+
           },
           err=>{
-            
+
           }
         )
       }
